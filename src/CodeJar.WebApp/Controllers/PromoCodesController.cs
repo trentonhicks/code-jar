2e19081c09b5a1099a5a09349f3d9a816be4d8ab
Steve Warren <steve-warren@users.noreply.github.com>
2019-11-21 23:07:45 -0600

    added web app scaffolding.

---
+++ src/CodeJar.WebApp/Controllers/PromoCodesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CodeJar.WebApp.Controllers
+{
+    [ApiController]
+    [Route("codes")]
+    public class PromoCodesController : ControllerBase
+    {
+        private readonly ILogger<PromoCodesController> _logger;
+
+        public PromoCodesController(ILogger<PromoCodesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new[] { "AAAAAA", "BBBBBB", "CCCCCC" };
+        }
+    }
+}
