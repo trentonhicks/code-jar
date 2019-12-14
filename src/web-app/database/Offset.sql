130fac887c9fa75c3bff15f41f5ca88ffcc45d54
Daniel Figueroa <Blackspade123@hotmail.com>
2019-12-13 19:08:04 -0600

    Added an insert statment to sql table offset to add values to ID and offsetvalue

--- src/web-app/database/Offset.sql
+++ src/web-app/database/Offset.sql
@@ -3,5 +3,7 @@ GO
 
 CREATE TABLE [dbo].[Offset](
 	ID int PRIMARY KEY NOT NULL,
-	OffsetValue bigint DEFAULT 0 NOT NULL,
-)
+	OffsetValue bigint DEFAULT 0 NOT NULL,);
+
+	INSERT INTO Offset (ID, OffsetValue)
+	VALUES (1,0);
