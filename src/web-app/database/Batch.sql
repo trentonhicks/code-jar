5e9536f42dc2383d4a1887eed66b36b05721c79c
Evan Nettleton <evangnettleton@gmail.com>
2019-12-17 21:25:25 -0600

    Update Sql script from BatchActive and BatchEnd to CodeIDStart and CodeIDEnd

--- src/web-app/database/Batch.sql
+++ src/web-app/database/Batch.sql
@@ -2,8 +2,8 @@ USE [RandomCode]
 GO
 CREATE TABLE [dbo].[Batch](
     ID int PRIMARY KEY IDENTITY(1,1) NOT NULL,
-	BatchStart int FOREIGN KEY REFERENCES Codes(ID) NOT NULL,
-	BatchEnd int FOREIGN KEY REFERENCES Codes(ID) NOT NULL,
-    [BatchSize] AS BatchEnd - BatchStart + 1, 
+	CodeIDStart int FOREIGN KEY REFERENCES Codes(ID) NOT NULL,
+	CodeIDEnd int FOREIGN KEY REFERENCES Codes(ID) NOT NULL,
+    [BatchSize] AS CodeIDStart - CodeIDEnd + 1, 
 	DateActive DATETIME NOT NULL,
 	DateExpires DATETIME NOT NULL)
