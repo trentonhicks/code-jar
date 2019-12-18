82253435d92b0d7962085165105ecc89b6af93b1
Evan Nettleton <evangnettleton@gmail.com>
2019-12-17 21:22:15 -0600

    Updated SQL script to include BatchStart, BatchEnd, BatchSize, DateActive, DateExpires

--- src/web-app/database/Batch.sql
+++ src/web-app/database/Batch.sql
@@ -2,6 +2,8 @@ USE [RandomCode]
 GO
 CREATE TABLE [dbo].[Batch](
     ID int PRIMARY KEY IDENTITY(1,1) NOT NULL,
-	CodeIDStart int FOREIGN KEY REFERENCES Codes(ID) NOT NULL,
-	CodeIDEnd int FOREIGN KEY REFERENCES Codes(ID) NOT NULL,
-    [BatchSize] int DEFAULT 0 NOT NULL);
+	BatchStart int FOREIGN KEY REFERENCES Codes(ID) NOT NULL,
+	BatchEnd int FOREIGN KEY REFERENCES Codes(ID) NOT NULL,
+    [BatchSize] AS BatchEnd - BatchStart + 1, 
+	DateActive DATETIME NOT NULL,
+	DateExpires DATETIME NOT NULL)
