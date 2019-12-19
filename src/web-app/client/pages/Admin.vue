5068d1112ef545ce8c56d7ffcfc72023ad027a3a
Trenton <trentonmhicks@gmail.com>
2019-12-19 10:21:30 -0600

    Batches can now be created from the front-end

--- src/web-app/client/pages/Admin.vue
+++ src/web-app/client/pages/Admin.vue
@@ -9,30 +9,64 @@
           
         .card-body
             .single-batch(v-if="batchSelected")
-                button.mb-2.btn.btn-sm.btn-outline-secondary(@click="BatchList()")
+                button.mb-2.btn.btn-sm.btn-outline-secondary(@click="GoBack()")
                     i.fa.fa-angle-left
                     |  Batch list
                 code-table
 
             //- Show batch list when batch isn't selected
             .batch-list(v-if="!batchSelected")
-                form.form-inline(v-on:submit.prevent='GenerateCodes()')
-                    input#batchName.form-control.form-control-sm.mr-sm-2.mb-2.mb-sm-0(type='text' placeholder='Batch name')
-                    input#startDate.form-control.form-control-sm.mr-sm-2.mb-2.mb-sm-0(type='text' onfocus="(this.type='date')" placeholder='Start Date')
-                    input#endDate.form-control.form-control-sm.mr-sm-2.mb-2.mb-sm-0(type='text' onfocus="(this.type='date')" placeholder='End Date')
-                    input#numberOfCodes.form-control.form-control-sm.mr-sm-2.mb-2.mb-sm-0(v-model='numberOfCodes' type='number' min="0" max="1000" placeholder='Number of codes')
+                form.form-inline(v-on:submit.prevent='CreateBatch()')
+                    
+                    //- Batch Name
+                    input#batchName.form-control.form-control-sm.mr-sm-2.mb-2.mb-sm-0(
+                        v-model="batchName"
+                        type='text'
+                        placeholder='Batch name'
+                        required)
+
+                    //- Date Active
+                    input#dateActive.form-control.form-control-sm.mr-sm-2.mb-2.mb-sm-0(
+                        v-model="dateActive" 
+                        type='text' onfocus="(this.type='date')" 
+                        placeholder='Start Date' 
+                        required)
+
+                    //- Date Expires
+                    input#dateExpires.form-control.form-control-sm.mr-sm-2.mb-2.mb-sm-0(
+                        v-model="dateExpires" 
+                        type='text' 
+                        onfocus="(this.type='date')" 
+                        placeholder='End Date')
+                    
+                    //- Batch Size
+                    input#batchSize.form-control.form-control-sm.mr-sm-2.mb-2.mb-sm-0(
+                        v-model='batchSize' 
+                        type='number' min="0"
+                        max="1000" 
+                        placeholder='Number of codes')
+
+                    //- Create Batch
                     button.btn.btn-sm.btn-primary(type='submit') Create Batch
 
-                .card(class="m-0 mt-3" v-for="batch in batchList" :key="batch.id" :disabled="batch.state == 'Inactive'")
+                //- Batch list
+                .card(
+                    class="m-0 mt-3" 
+                    v-for="batch in batches" 
+                    :key="batch.id" 
+                    :disabled="batch.state == 'Inactive'")
                     .card-body
                         .row
                             .col-md
-                                h5.card-title.mb-1.mb-md-3 {{ batch.name }}
-                                span.badge.badge-primary.mr-2 52 codes
-                                span.badge.badge-secondary Expires on 12/17/19
+                                h5.card-title.mb-1.mb-md-3 {{ batch.batchName }}
+                                span.badge.badge-primary.mr-2 {{ batch.batchSize }} codes
+                                span.badge.badge-secondary Expires on {{ batch.dateExpires }}
                             .col-md.mt-3.mt-md-0
                                 button.btn.btn-sm.btn-block.btn-outline-primary(@click="ViewBatch()") View Codes
                                 button.btn.btn-sm.btn-block.btn-outline-danger() Deactivate
+
+                //- Alert when there are no batches
+                p.text-muted.mt-3.mb-0(v-if="batches.length == 0" role="alert") No batches have been created yet.
 
 </template>
 
@@ -45,51 +79,43 @@ module.exports = {
     name: 'Admin',
     data: function() {
         return {
-            numberOfCodes: 0,
             batchSelected: false,
-            batchList: [
-                {
-                    id: 1,
-                    name: "Batch Name",
-                    state: "Active"
-                },
-                {
-                    id: 2,
-                    name: "Batch Name",
-                    state: "Active"
-                },
-                {
-                    id: 3,
-                    name: "Batch Name",
-                    state: "Active"
-                },
-            ]
+            batchName: '',
+            batchSize: 0,
+            dateActive: '',
+            dateExpires: '',
+            batches: []
         }
     },
     components: {
         CodeTable
     },
     methods: {
-        GenerateCodes() {
-            if(this.numberOfCodes > 0) {
+        CreateBatch() {
+            if(this.batchSize > 0) {
                 HTTP({
                     method: 'post',
-                    url: 'codes',
-                    data: this.numberOfCodes,
+                    url: 'batchcodes',
+                    data: {
+                        batchName: this.batchName,
+                        batchSize: parseInt(this.batchSize),
+                        dateActive: this.dateActive,
+                        dateExpires: this.dateExpires,
+                    },
                     headers: {
                         'Content-Type': 'application/json'
                     }
                 }).then(response => {
                     // Show new batch in the list of batches
                 }).catch(e => {
-                    // Unable to generate codes
+                    // Unable to create batch
                 });
             }
         },
         ViewBatch() {
             this.batchSelected = true;
         },
-        BatchList() {
+        GoBack() {
             this.batchSelected = false;
         }
     },
