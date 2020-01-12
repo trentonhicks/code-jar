56d6c04f6daa8a85f7590d957e5e1337d17c138c
Daniel Figueroa <Blackspade123@hotmail.com>
2020-01-11 19:32:31 -0600

    Renamed Datecheck to JobDateCheck. Updated the deploy to run job.

--- src/web-app/database/deploy.ps1
+++ src/web-app/database/deploy.ps1
@@ -3,4 +3,5 @@ Invoke-Sqlcmd -InputFile RandomCode.sql
 Invoke-Sqlcmd -InputFile Codes.sql
 Invoke-Sqlcmd -InputFile Batch.sql
 Invoke-Sqlcmd -InputFile Offset.sql
-Invoke-Sqlcmd -InputFile AdminLogin.sql
+Invoke-Sqlcmd -InputFile AdminLogin.sql 
+Invoke-Sqlcmd -InputFile JobDateCheck.sql
