3111172c0edc7d71b45ce0d4b8a169b4450a7bdd
Steve Warren <steve-warren@users.noreply.github.com>
2020-01-21 18:22:01 -0600

    Gave CodeIdStart and End a unique id

--- src/web-app/database/Batch.sql
+++ src/web-app/database/Batch.sql
@@ -3,8 +3,8 @@ GO
 CREATE TABLE [dbo].[Batch](
     ID int PRIMARY KEY IDENTITY(1,1) NOT NULL,
 	BatchName varchar(50) NOT NULL,
-	CodeIDStart int NOT NULL,
-	CodeIDEnd int NOT NULL,
+	CodeIDStart int unique NOT NULL,
+	CodeIDEnd int unique NOT NULL,
     [BatchSize] AS CodeIDEnd - CodeIDStart + 1,
 	DateActive DATETIME NOT NULL,
 	DateExpires DATETIME NOT NULL)
