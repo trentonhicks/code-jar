33c9d4cf4e32a667f83476fcd1b385aa6d9b1582
Steve Warren <steve-warren@users.noreply.github.com>
2020-01-28 20:53:48 -0600

    Add first test -Trenton

--- src/api-tester/Program.cs
+++ src/api-tester/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace api_tester
 {
@@ -6,7 +9,30 @@ namespace api_tester
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           var id = CreateBatchAsync().Result;
+           GetBatchAsync(id).Wait();
+
+        }
+
+        static async Task GetBatchAsync(int batchID)
+        {
+            var http = new HttpClient();
+            var response = await http.GetStringAsync($"http://localhost:5000/batch/{batchID}?page=1");
+            Console.WriteLine(response);
+        }
+
+        static async Task<int> CreateBatchAsync()
+        {
+            var http = new HttpClient();
+            var payload = "{\"BatchName\": \"foo\",\"BatchSize\": 20, \"DateActive\": \"2020-01-28\", \"DateExpires\": \"2020-01-29\"}";
+            HttpContent foo =  new StringContent(payload, Encoding.UTF8, "application/json");
+
+            var response = await http.PostAsync("http://localhost:5000/batch", foo);
+
+            var content = Convert.ToInt32(await response.Content.ReadAsStringAsync());
+            Console.WriteLine(content);
+            return content;
+
         }
     }
 }
