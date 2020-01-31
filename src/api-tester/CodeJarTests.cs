a578ecdf792c3faa066734ba444110e0295b4cbe
unknown <plwieseler@gmail.com>
2020-01-31 12:05:42 -0600

    Added test for cheking if the states are correct for code creation.

--- src/api-tester/CodeJarTests.cs
+++ src/api-tester/CodeJarTests.cs
@@ -9,6 +9,37 @@ namespace api_tester
 {
     public class CodeJarTests
     {
-        
+
+        private static CodeJarClient _codeJarClient = new CodeJarClient(_jsonOptions);
+        private static JsonSerializerOptions _jsonOptions = new JsonSerializerOptions {
+            PropertyNameCaseInsensitive = true,
+        };
+
+        public static async Task<bool> IsCodeStateCorrect (Batch batch, int batchID)
+        {
+
+            var getCodes = await _codeJarClient.GetBatchAsync(batchID);
+
+            var code = getCodes.Codes[0];
+
+            if(batch.DateActive <= DateTime.Now)
+            {
+                if(code.State == "Active")
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                if(code.State == "Generated")
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+
     }
 }
